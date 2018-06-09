e220ca75a3f3f2744c7708a63e97a863d53eb6f9
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-06-09 18:41:39 +0300

    mobile version

--- src/app/shared/time-line-cronology/time-line-cronology.component.ts
+++ src/app/shared/time-line-cronology/time-line-cronology.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input,AfterViewInit } from '@angular/core';
 })
 export class TimeLineCronologyComponent implements OnInit, AfterViewInit {
   @Input('event') event;
-  private duration;
+  public duration;
 
   constructor() { }
 
@@ -15,34 +15,31 @@ export class TimeLineCronologyComponent implements OnInit, AfterViewInit {
     if(this.event.periods.start){
       this.duration = this.days_between(this.event.periods.end, this.event.periods.start);
       this.duration = this.durationToView(this.duration);
-      // console.log(this.duration);
-      
     }
-    
   }
 
   ngAfterViewInit(){
   }
 
-  days_between(date1, date2) {  
+  days_between(date1, date2) {
 
     // The number of milliseconds in one day
-    var ONE_DAY = 1000 * 60 * 60 * 24
+    const ONE_DAY = 1000 * 60 * 60 * 24;
 
     // Convert both dates to milliseconds
-    var date1_ms = date1.getTime()
-    var date2_ms = date2.getTime()
+    const date1_ms = date1.getTime();
+    const date2_ms = date2.getTime();
 
     // Calculate the difference in milliseconds
-    var difference_ms = Math.abs(date1_ms - date2_ms)
+    const difference_ms = Math.abs(date1_ms - date2_ms);
 
     // Convert back to days and return
-    return Math.round(difference_ms/ONE_DAY)
+    return Math.round(difference_ms/ONE_DAY);
   }
 
-  durationToView(days){
-    let years = Math.floor(days/365);
-    let months = Math.floor(days%365/30);
+  durationToView(days) {
+    const years = Math.floor(days / 365);
+    const months = Math.floor(days % 365 / 30);
     // console.log('y=>', years, 'm=>', months);
     return {years: years, months: months};
   }
